3466d3084ebd02b39f6dee78a0b1e56e35965aee
NolikUss <83816117+NolikUss@users.noreply.github.com>
2021-12-24 12:13:23 +0400

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,16 @@
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
+
+client.login('OTEyMDAwNTg3NTY0OTc0MDkw.YZpkvA._L0iXx8LE1G92JyV_GD1oO_iXAk');
